cbb368a83bf00a3e26e9dc960a169bf1b21afc72
soulsf <solsfelder@googlemail.com>
2019-02-22 21:08:20 +0100

    Update rt_ticket.py

--- responders/RT_Ticket/rt_ticket.py
+++ responders/RT_Ticket/rt_ticket.py
@@ -5,10 +5,15 @@ from cortexutils.responder import Responder
 from rtkit.resource import RTResource
 from rtkit.authenticators import CookieAuthenticator
 from rtkit.errors import RTResourceError
+from thehive4py.api import TheHiveApi
+from thehive4py.models import Case, CaseTask, CustomFieldHelper
+
+
 
 from rtkit import set_logging
 import logging
 import re
+import time
 
 set_logging('debug')
 logger = logging.getLogger('rtkit')
@@ -22,9 +27,16 @@ class RT(Responder):
         self.rt_queue= self.get_param('config.rt_queue', '')
         self.rt_subject= self.get_param('config.rt_subject', '')
         self.rt_thid= self.get_param('config.rt_thid', 'internal ID')
+        self.rt_text = self.get_param('config.rt_text', '')
         self.rt_pw = self.get_param('config.rt_pw', None, '')
+        self.th_api_key = self.get_param('config.th_apikey', None, '')
+        self.th_url= self.get_param('config.th_url', None, '')
+
 
         self.resource = RTResource(self.rt_url, self.rt_user, self.rt_pw, CookieAuthenticator)
+
+        self.api = TheHiveApi(self.th_url, self.th_api_key )
+
 
 
     def run(self):
@@ -37,6 +49,13 @@ class RT(Responder):
         description = description.encode('utf-8')
 
         if self.data_type == 'thehive:case':
+            # die if ticket created
+            tags = self.get_param('data.tags', None, '')
+            rt_tag = [t[11:] for t in tags if t.startswith('rt_ticket:')]
+            if rt_tag:
+                self.error('RT ticket already exists.')
+
+
             # Search requestor in custom field rt_req
             req = self.get_param('data.customFields.rt_req.string', None, 'requestor not found in customfield rt_req')
             if req:
@@ -49,6 +68,7 @@ class RT(Responder):
         caseid = self.get_param('data._id')
         casenr = str(self.get_param('data.caseId'))
         subject = self.rt_subject
+        text = self.rt_text.encode('utf-8')
 
         if '__thehivetitle__' in subject:
             subject = subject.replace('__thehivetitle__', title)
@@ -58,9 +78,10 @@ class RT(Responder):
 
         content = {
             'content': {
-            'Queue': self.rt_queue,
+            'Queue': self.rt_queue, 
             'Subject' : subject,
             'Requestor': req,
+            'Text': text,
             'CF.{'+self.rt_thid+'}' : caseid
             }
             }
@@ -72,6 +93,36 @@ class RT(Responder):
             logger.error(e.response.status_int)
             logger.error(e.response.status)
             logger.error(e.response.parsed)
+
+        # Prepare the custom fields
+        casecfields = self.get_param('data.customFields', None)
+        # get existing Fields
+        tmp_cf_assemble = CustomFieldHelper()
+        for eachcf in casecfields.items():
+            if 'string' in eachcf[1]:
+                tmp_cf_assemble.add_string(str(eachcf[0]), eachcf[1]['string'])
+            elif 'date' in eachcf[1]:
+                tmp_cf_assemble.add_date(str(eachcf[0]), eachcf[1]['date'])
+            elif 'boolean' in eachcf[1]:
+                tmp_cf_assemble.add_boolean(str(eachcf[0]), eachcf[1]['boolean'])
+            elif 'number' in eachcf[1]:
+                tmp_cf_assemble.add_number(str(eachcf[0]), eachcf[1]['number'])
+
+        # modify customField
+        tmp_cf_assemble.add_string('escalated', 'yes')
+        set_customFields = tmp_cf_assemble.build()
+
+        updated_case = self.api.case.update(caseid,
+                customFields=set_customFields
+                )
+
+        update_case = self.api.create_case_task(caseid, CaseTask(
+        title='Customer communication RT Ticket ID #{}'.format(id[0]),
+        status='InProgress',
+        owner='admin',
+        flag=True,
+        startDate=int(time.time())*1000))
+
         self.report({'rt_ticket' : id[0]})
 
     def operations(self, raw):
@@ -80,3 +131,4 @@ class RT(Responder):
 
 if __name__ == '__main__':
     RT().run()
+    
